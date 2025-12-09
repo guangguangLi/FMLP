9ee98adc5c8251dabaaf3f2cfdbc813e63f3fb43
GuangjuLi <104249876+guangguangLi@users.noreply.github.com>
2025-12-09 11:11:03 +0800

    Add files via upload

---
+++ metrics.py
@@ -0,0 +1,54 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+
+def iou_score(output, target):
+    smooth = 1e-5
+
+    if torch.is_tensor(output):
+        output = torch.sigmoid(output).data.cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.data.cpu().numpy()
+    output_ = output > 0.5
+    target_ = target > 0.5
+    intersection = (output_ & target_).sum()
+    union = (output_ | target_).sum()
+    iou = (intersection + smooth) / (union + smooth)
+    dice = (2* iou) / (iou+1)
+    
+    TP = intersection
+    FP = (output_ & ~target_).sum()
+    FN = (~output_ & target_).sum()
+    
+    # Calculation of precision and recall
+    precision = (TP + smooth) / (TP + FP + smooth)
+    recall = (TP + smooth) / (TP + FN + smooth)
+    
+    return iou, dice, precision,recall
+
+
+def Iou_score(output, target):
+    smooth = 1e-5
+
+    if torch.is_tensor(output):
+        output = torch.sigmoid(output).data.cpu().numpy()
+    if torch.is_tensor(target):
+        target = target.data.cpu().numpy()
+    output_ = output > 0.5
+    target_ = target > 0.5
+    intersection = (output_ & target_).sum()
+    union = (output_ | target_).sum()
+    iou = (intersection + smooth) / (union + smooth)
+    dice = (2* iou) / (iou+1)
+    return iou, dice
+
+def dice_coef(output, target):
+    smooth = 1e-5
+
+    output = torch.sigmoid(output).view(-1).data.cpu().numpy()
+    target = target.view(-1).data.cpu().numpy()
+    intersection = (output * target).sum()
+
+    return (2. * intersection + smooth) / \
+        (output.sum() + target.sum() + smooth)
